bebf1100acde845650cbaab428583fbdd0c7de19
HP <montassar.chihi@glovoapp.com>
2024-03-06 08:29:43 +0100

    added admin functions

--- Admin.py
+++ Admin.py
@@ -9,25 +9,40 @@ class Admin:
         self.refresh_token = refresh_token
 
     def rebook_kickedout_couriers(self, file=None, courier_id=None, slot_id=None):
+        message = "فرصة أخرى بش تعمل check-in !! :warning: فما برشة خدمة  :motor_scooter: فرصة بش دخل أكثر فلوس !!  :money_with_wings:"
+        couriers_ids = []
         if (courier_id is None) and (slot_id is None):
             df = pd.read_csv(file)
-
             for index, row in df.iterrows():
                 courier_id = row["courier_id"]
+                couriers_ids.append(courier_id)
                 slot_id = row["slot_id"]
                 self.unbook_slot(courier_id, slot_id)
-                r = self.book_slot(courier_id, slot_id)
-                print("courier_id: ", courier_id, "slot_id: ", slot_id, "successful booking: ", r)
-        else:
-            self.unbook_slot(courier_id, slot_id)
-            r = self.book_slot(courier_id, slot_id)
-            print("courier_id: ", courier_id, "slot_id: ", slot_id, "successful booking: ", r)
+                self.book_slot(courier_id, slot_id)
+            self.send_push_notification(couriers_ids,message)
 
-    def setup_new_couriers_profiles(self):
-        new_couriers = pd.read_csv("data/Fleet Overview - TIS - Formation.csv")
-        new_couriers = new_couriers[new_couriers["courier_id"].isna() == False]
+        else:
+            couriers_ids = [courier_id]
+            self.unbook_slot(courier_id, slot_id)
+            self.book_slot(courier_id, slot_id)
+            self.send_push_notification(couriers_ids, message)
+
+    def send_push_notification(self,couriers_ids,message):
+        link = f"https://adminapi.glovoapp.com/admin/notifications/couriers"
+        data = {
+            "usersIds": couriers_ids,
+            "body": message,
+        }
+        try:
+            response = requests.post(link, headers={'Content-Type': 'application/json', 'authorization': self.token}, json=data).json()
+            return True,response
+        except:
+            return False
+
+    def setup_new_couriers_profiles(self, file):
+        new_couriers = pd.read_csv(file)
         for index, courier_data in new_couriers.iterrows():
-            courier_id = int(courier_data["courier_id"])
+            courier_id = int(self.search("courier", courier_data["EMAIL"]))
             new_bag_tag = 1417
             threepl_tag = courier_data["threepl_tag_id"]
             threepl_id = courier_data["threepl_id"]
@@ -120,3 +135,104 @@ class Admin:
                 return False, r.text
         except Exception:
             return False, 'Error book_slot'
+
+    def get_courier_kickouts(self, courier_id):
+        link = "https://adminapi.glovoapp.com/admin/couriers/"+ str(courier_id) +"/kickouts?limit=500&page=0"
+        r = requests.get(link,
+                         headers={'Content-Type': 'application/json',
+                                  'authorization': self.token}).json()
+        kickouts = r["content"]
+        kickouts_df = pd.DataFrame(kickouts)
+        return kickouts_df
+
+    def get_courier_performance(self, courier_id):
+
+        return
+
+    def get_order_details(self, order_id):
+        link = "https://adminapi.glovoapp.com/admin/orders/"+str(order_id)+"/info"
+        r = requests.get(link, headers={'Content-Type': 'application/json', 'authorization': self.token},).json()
+        return r
+
+    def search(self, field, query):
+        link = "https://adminapi.glovoapp.com/admin/"+str(field)+"s/search"
+        data = {
+            "cities": ["BES", "BIZ", "GBS", "HMN", "HMS", "NBL", "NSO", "SFX", "SOU", "TIE", "TIS"],
+            "query": query
+        }
+        r = requests.post(link,
+                         headers={'Content-Type': 'application/json',
+                                  'authorization': self.token},
+                          json=data).json()
+        if field == "order":
+            id_result = r["orderCodeResults"]
+        elif field == "courier":
+            if r["courierEmailResults"] is not None:
+                courier_to_get = len(r["courierEmailResults"]) - 1
+                id_result = r["courierEmailResults"][courier_to_get]["id"]
+            elif r["courierIdResults"] is not None:
+                courier_to_get = len(r["courierIdResults"]) - 1
+                id_result = r["courierIdResults"][courier_to_get]["id"]
+            elif r["courierNameResults"] is not None:
+                courier_to_get = len(r["courierNameResults"]) - 1
+                id_result = r["courierNameResults"][courier_to_get]["id"]
+            elif r["courierPhoneResults"] is not None:
+                courier_to_get = len(r["courierPhoneResults"]) - 1
+                id_result = r["courierPhoneResults"][courier_to_get]["id"]
+
+        return id_result
+
+    # reasons needs to be predefined like those in admin panel
+    def courier_enable(self, courier_id, reason):
+        try:
+            json_admin_data = {
+                "status": "ENABLED",
+                "statusReason": reason
+            }
+            # post updated info
+            link = 'https://adminapi.glovoapp.com/admin/couriers/' + str(courier_id) + '/secondary_settings'
+            r = requests.put(link,
+                             headers={'Content-Type': 'application/json',
+                                      'authorization': self.token},
+                             json=json_admin_data).json()
+            return r
+        except Exception:
+            raise Exception('Something went wrong, impossible enable courier')
+
+    def courier_block(self, courier_id, reason, duration=None):
+        try:
+            json_admin_data = {
+                "status": "BLOCKED",
+                "statusReason": reason
+            }
+            if duration is not None:
+                json_admin_data = {
+                    "status": "BLOCKED",
+                    "statusReason": reason,
+                    "duration": duration
+                }
+            # post updated info
+            link = 'https://adminapi.glovoapp.com/admin/couriers/' + str(courier_id) + '/secondary_settings'
+            r = requests.put(link,
+                             headers={'Content-Type': 'application/json',
+                                      'authorization': self.token},
+                             json=json_admin_data).json()
+            return r
+        except Exception:
+            raise Exception('Something went wrong, impossible to block courier')
+
+    def courier_disable(self, courier_id, reason):
+        try:
+            json_admin_data = {
+                "status": "DISABLED",
+                "statusReason": reason
+            }
+            # post updated info
+            link = 'https://adminapi.glovoapp.com/admin/couriers/' + str(courier_id) + '/secondary_settings'
+            r = requests.put(link,
+                             headers={'Content-Type': 'application/json',
+                                      'authorization': self.token},
+                             json=json_admin_data).json()
+            return r
+        except Exception:
+            raise Exception('Can\'t disable courier')
